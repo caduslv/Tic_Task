fcc14385a27e0ced532907543ae889ceded167c6
caduslv <calos.edu86@gmail.com>
2026-06-22 00:30:15 -0300

    fix: adiciona suporte a porta dinâmica e altera banco para pool

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,36 +1,28 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
 
-
-
-
 dotenv.config();
 
-
-
-
-const connection = mysql.createConnection({
+// Alterado para createPool para suportar os testes de concorrência do k6
+const connection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306, // 💡 CRÍTICO para o deploy na nuvem
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-
-
-
-connection.connect((err) => {
+// Executa o teste inicial usando o Pool adaptado
+connection.query('SELECT DATABASE() AS banco', (err, result) => {
   if (err) {
-    console.error('Erro ao conectar ao banco de dados:', err);
+    console.error('❌ Erro ao conectar ao banco de dados:', err);
     return;
   }
-  console.log('Conexão bem-sucedida com o banco de dados MySQL!');
-});
-
-
-connection.query('SELECT DATABASE() AS banco', (err, result) => {
+  console.log('✅ Conexão bem-sucedida com o banco de dados MySQL!');
   console.log('📦 BANCO USADO PELO NODE:', result[0].banco);
 });
-
 
 export default connection;
